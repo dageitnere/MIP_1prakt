239a0c2712872c753d81c0c472663c3c2e9284fc
ReinisJunga <reinis.junga@edu.rtu.lv>
2025-03-31 23:32:53 +0300

    Izņemta testu klase ārpus foldera

--- AlgoTest.py
+++ AlgoTest.py
@@ -5,7 +5,7 @@ from HeuristicFunction import minMax, alphaBeta
 
 testNumbers = [13776, 18438, 16506, 19128, 17640, 10524, 14184, 19890, 11664, 16092]
 
-def average_runtime(algorithm, depth, numbers, trials=10):
+def averageRuntime(algorithm, depth, numbers, trials=10):
     times = []
     for num in numbers:
         startNumber = RunSettings(1, "AlfaBeta", num)
@@ -26,6 +26,6 @@ trials = 10
 print("Izmantojot desmit dazadus ciparus, datora videjais lemuma pienemsanas laiks atkariba no dziluma:")
 
 for depth in depths:
-    minmax_avg = average_runtime(minMax, depth, testNumbers, trials)
-    alphabeta_avg = average_runtime(alphaBeta, depth, testNumbers, trials)
+    minmax_avg = averageRuntime(minMax, depth, testNumbers, trials)
+    alphabeta_avg = averageRuntime(alphaBeta, depth, testNumbers, trials)
     print(f"Dzilums {depth}: MinMax = {minmax_avg:.10f}s, AlphaBeta = {alphabeta_avg:.10f}s")
